b2197e49a195830d6a38a9bb956c8dd58f8759f9
Adriano D'Alonzo <adrianodalonzo@gmail.com>
2023-05-19 15:49:13 -0400

    created forms for login and signup

---
+++ StudyBuddyFinder/objects/forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, EmailField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+
+class SignupForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = EmailField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    retype_password = PasswordField('Retype Password', validators=[DataRequired()])
--- StudyBuddyFinder/views/auth_views.py
+++ StudyBuddyFinder/views/auth_views.py
@@ -1,11 +1,25 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, request, url_for
+
+from ..objects.forms import LoginForm, SignupForm
 
 auth_bp = Blueprint('auth', __name__, url_prefix='/auth/')
 
-@auth_bp.route('/login/')
+@auth_bp.route('/login/', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            return render_template('login.html', form=form)
+        return redirect(url_for('home.home'))
+    elif request.method == 'GET':
+        return render_template('login.html', form=form)
 
 @auth_bp.route('/signup/')
 def signup():
-    return render_template('signup.html')
+    form = SignupForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            return render_template('signup.html', form=form)
+        return redirect(url_for('home.home'))
+    elif request.method == 'GET':
+        return render_template('signup.html', form=form)
